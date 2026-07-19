db266a14f137ebda72971263d34cd5331d26fda9
Julen Garcia Manterola <julengarciaaa@gmail.com>
2026-07-19 11:23:08 +0200

    Create script to launch the baseline model test.

---
+++ src/model/.ipynb_checkpoints/piece-checkpoint.py
@@ -0,0 +1,56 @@
+import copy
+
+class Piece:
+    __slots__ = ["length", "width", "packed_value"]
+
+    def __init__(self, length, width, packed_value):
+        self.length = length
+        self.width = width
+        self.packed_value = packed_value
+    
+    def get_length(self):
+        return self.length
+        
+    def get_width(self):
+        return self.width
+    
+    def get_area(self):
+        return self.length * self.width
+    
+    def get_packed_value(self):
+        return self.packed_value
+    
+    def clone(self):
+        return copy.copy(self)
+    
+    @staticmethod
+    def piece_to_dict(piece):
+        return {
+            "length": piece.length,
+            "width": piece.width,
+            "packed_value": piece.packed_value
+        }
+
+    @staticmethod
+    def piece_from_dict(d):
+        return Piece(
+            length=d["length"],
+            width=d["width"],
+            packed_value=d["packed_value"]
+        )
+
+    
+    def __eq__(self, other):
+        if not isinstance(other, Piece):
+            return False
+        
+        return ((self.length == other.length and self.width == other.width or
+                self.length == other.width and self.width == other.length) and 
+                self.packed_value == other.packed_value)
+    
+    def __hash__(self):
+        dimensions = (min(self.length, self.width), max(self.length, self.width))
+        return hash((dimensions, self.packed_value))
+
+    def __repr__(self):
+        return f"Piece({self.length}x{self.width})"
---
+++ src/model/.ipynb_checkpoints/placement-checkpoint.py
@@ -0,0 +1,61 @@
+import copy
+from src.model.piece import Piece
+
+class Placement:
+    __slots__ = ["piece", "p_point", "_is_rotated"]
+
+    def __init__(self, piece, is_rotated, p_point):
+        self.piece = piece
+        self._is_rotated = is_rotated
+        self.p_point = p_point
+
+    def get_piece(self):
+        return self.piece
+    
+    def get_p_point(self):
+        return self.p_point
+    
+    def get_width(self):
+        return self.piece.get_length() if self._is_rotated else self.piece.get_width()
+        
+    def get_length(self):
+        return self.piece.get_width() if self._is_rotated else self.piece.get_length()
+        
+    def get_area(self):
+        return self.get_length() * self.get_width()
+
+    def is_rotated(self):
+        return self._is_rotated
+    
+    def clone(self):
+        return copy.copy(self)
+    
+    def placement_to_dict(pl):
+        return {
+            "piece": Piece.piece_to_dict(pl.piece),
+            "p_point": tuple(pl.p_point),
+            "is_rotated": pl._is_rotated
+        }
+    
+    def placement_from_dict(d):
+        piece = Piece.piece_from_dict(d["piece"])
+        p_point = tuple(d["p_point"])
+        is_rotated = d["is_rotated"]
+        return Placement(piece, is_rotated, p_point)
+
+
+
+    def __eq__(self, other):
+        if not isinstance(other, Placement):
+            return False
+        
+        return (self.piece == other.piece and 
+                self._is_rotated == other._is_rotated and 
+                self.p_point == other.p_point)
+
+    def __hash__(self):
+        return hash((self.piece, self._is_rotated, self.p_point))
+
+    def __repr__(self):
+        rot = "Rotated" if self._is_rotated else "Normal"
+        return f"Placement({self.piece} at {self.p_point}, {rot})"
